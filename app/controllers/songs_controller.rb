4e90915966057cb425a13367a42e8e4bd7666ba9
danghanh <ldhanh1999@gmail.com>
2020-06-08 09:21:01 +0700

    new song without file mp3

---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,30 @@
+class SongsController < ApplicationController
+  def new
+    @song = Song.new()
+  end
+
+  def create 
+    byebug
+    @song = current_user.songs.build(song_params) if admin?
+    category = Category.find_by name: params[:song][:category]
+    @song.category_id = category.id
+    if @song.save
+      flash[:success] = "song created!"
+      redirect_to song_path(@song)
+    else
+      render "songs/new"
+    end
+  end
+
+  def show
+    @song = Song.find(params[:id]) 
+    raise ActiveRecord::RecordNotFound unless @song
+    @song
+  end
+  private
+
+  def song_params
+    params.require(:song).permit(:name, :artist, :lyric)
+  end
+
+end
